8953d336e6766bee0bdfa1d8965ee383d9f1cd69
Rashadul-hub <rashadulislamhimu@gmail.com>
2024-05-21 01:58:25 +0600

    (Complete)Change App Theme (Provider)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_flutter/example_screens/favourite/favourite_screen.dart';
-import 'package:provider_flutter/example_screens/sliderColor_change_example.dart';
+
 import 'package:provider_flutter/provider/count_provider.dart';
 import 'package:provider_flutter/provider/favourite_provider.dart';
 import 'package:provider_flutter/provider/slider_color_change_provider.dart';
+import 'package:provider_flutter/provider/theme_changer_provider.dart';
+
+import 'example_screens/theme/dark_theme_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,28 +18,51 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MultiProvider(
-
       providers: [
         ChangeNotifierProvider(create: (_) => CountProvider()),
         ChangeNotifierProvider(create: (_) => SliderColorChangeProvider()),
         ChangeNotifierProvider(create: (_) => FavouriteProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeChangerProvider()),
       ],
+      child: Builder(builder: (BuildContext context) {
+        //Initialize ThemeChanger Provider Class
+        final themeChangerProvider = Provider.of<ThemeChangerProvider>(context);
 
-      child:MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
 
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home:  const FavouriteScreen(),
-      ) ,
+          //Add Provider for Theme Mode
+          themeMode: themeChangerProvider.themeMode,
+
+          //Themes Demo  For Light Mode
+          theme: ThemeData(
+            brightness: Brightness.light,
+            colorScheme: ColorScheme.light(
+              primary: Colors.lightBlueAccent,
+             ),
+            appBarTheme: AppBarTheme(
+              backgroundColor: Colors.blue
+            ),
+            useMaterial3: true,
+          ),
+
+          //Themes Demo For Dark Mode
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            colorScheme: const ColorScheme.dark(
+              primary: Colors.red
+            ),
+            appBarTheme: AppBarTheme(
+                backgroundColor: Colors.red
+            ),
+            useMaterial3: true,
+          ),
+
+          home: const DarkThemeScreen(),
+        );
+      }),
     );
-
-
-
   }
 }
